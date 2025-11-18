82b0a7592a3c356ce67581833645c303ff330de6
Ismael Fernandez <ismael-fernandez@outlook.es>
2025-11-18 16:07:39 +0100

    Añadido schema del input de producto con validación - product_id debe ser un entero positivo - stores_id debe ser o un número o seguir el formato "d|d|d...", donde d es un número - title debe ser de tipo string y que no sea una cadena vacía - price debe ser float y positivo

---
+++ src/schemas/product_input.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field, PositiveInt, PositiveFloat
+
+class ProductInput(BaseModel):
+    product_id: PositiveInt = Field(..., description="Identificador del producto")
+    # REGEX: Detecta una cadena de dígitos separados por '|', por ejemplo: "1|2|3"
+    # o simplemente un dígito "1"
+    stores_id: str = Field(..., pattern="^\d+(?:\|\d+)*$", description="Identificador de la tienda")
+    title: str = Field(..., description="Título del producto", min_length=1)
+    price: PositiveFloat = Field(..., description="Precio del producto")
